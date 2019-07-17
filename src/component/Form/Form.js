81a239d31d642953505332d662c7e702628b9ccd
Kevin Veiga <kevin.veiga@gmail.com>
2019-07-17 10:40:13 -0300

    ajuste

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,14 +1,14 @@
-import React, { Fragment, forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 
 import { Svg } from '../Svg/Svg';
 
 export const Input = forwardRef(({ classNameInput = 'custom-input', classNameValidate = 'icon-right', error = '', touched, typeInput = 'text', value = '', ...otherProps }, ref) => {
     return (
-        <Fragment>
+        <>
             <input autoComplete="off" className={`${classNameInput} ${error ? 'invalid' : `${touched.find((item) => item === otherProps.name) ? 'valid' : ''}`}`} defaultValue={value} ref={ref} type={typeInput} {...otherProps} />
 
             <Svg className={`${classNameValidate} ${error ? 'svg-invalid' : 'svg-valid'}`} name={error ? 'svg-invalid' : 'svg-valid'} />
-        </Fragment>
+        </>
     );
 });
 
