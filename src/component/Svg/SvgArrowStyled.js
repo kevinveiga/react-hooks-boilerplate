a4c04fedab3c2f19e4e2df5b61631fd185257dbd
Kevin Veiga <kevin.veiga@gmail.com>
2019-08-14 14:39:18 -0300

    update styled do svg

--- src/component/Svg/SvgArrowStyled.js
+++ src/component/Svg/SvgArrowStyled.js
@@ -4,6 +4,5 @@ import { variable } from '../../style/variable';
 
 export const SvgArrowStyled = styled.svg`
     fill: ${variable.colorPrimary};
-    height: 18px;
-    vertical-align: sub;
+    height: 16px;
 `;
