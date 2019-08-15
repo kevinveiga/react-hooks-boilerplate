4c6432f246213e818fe529ff71fa20caac064b43
Kevin Veiga <kevin.veiga@gmail.com>
2019-08-15 15:46:06 -0300

    update

--- src/util/customValidate.js
+++ src/util/customValidate.js
@@ -9,16 +9,25 @@ const validateMessage = {
 };
 
 export const customValidate = {
+    amount: {
+        pattern: { message: validateMessage.AMOUNT, value: /^\d{0,20}(,\d{1,2})?$/ }
+    },
+    area: {
+        pattern: { message: validateMessage.AMOUNT, value: /\d+/ }
+    },
     email: {
-        pattern: { message: validateMessage.EMAIL, value: /^(([^<>()[\]{}\\.,;:\s@"]+(\.[^<>()[\]{}\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ },
-        required: validateMessage.REQUIRED
+        pattern: { message: validateMessage.EMAIL, value: /^(([^<>()[\]{}\\.,;:\s@"]+(\.[^<>()[\]{}\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ }
+    },
+    month: {
+        pattern: { message: validateMessage.MONTH, value: /\d+/ }
     },
     name: {
-        minLength: { message: 'Mínimo de 5 caracteres', value: 5 },
-        required: validateMessage.REQUIRED
+        minLength: { message: 'Mínimo de 5 caracteres', value: 5 }
     },
     phone: {
-        pattern: { message: validateMessage.PHONE, value: /^\(?(\d{2})\)?\s?(\d{4}|\d{5})-?(\d{4})$/ },
+        pattern: { message: validateMessage.PHONE, value: /^\(?(\d{2})\)?\s?(\d{4}|\d{5})-?(\d{4})$/ }
+    },
+    require: {
         required: validateMessage.REQUIRED
     }
 };
