e24b87e681675de4dda204adeae10267f6b76dfa
Kevin Veiga <kevin.veiga@gmail.com>
2019-08-21 17:16:25 -0300

    fix state do video

--- src/store/video/video.js
+++ src/store/video/video.js
@@ -1,5 +1,4 @@
 import { useLayoutEffect, useState } from 'react';
-import { isMobile } from 'react-device-detect';
 
 import { scrollTo } from '../../util/scrollTo';
 
@@ -7,10 +6,6 @@ export const useCurrentVideo = (obj, ancorHash, offset) => {
     const [stateCurrentVideo, setStateCurrentVideo] = useState(obj);
 
     useLayoutEffect(() => {
-        if (isMobile) {
-            return undefined;
-        }
-
         scrollTo(ancorHash, offset);
 
         return undefined;
