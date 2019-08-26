13309b8a14053595cfe28b55740afec3fdd7a869
Kevin Veiga <kevin.veiga@gmail.com>
2019-08-26 11:22:06 -0300

    update scrollTo

--- src/util/scrollTo.js
+++ src/util/scrollTo.js
@@ -1,25 +1,44 @@
-let waitScroll = false;
+let scrollOnce = false;
+
+const fnScroll = (ancor) => {
+    try {
+        window.scroll({
+            behavior: 'smooth',
+            left: 0,
+            top: ancor
+        });
+    } catch (error) {
+        window.scrollTo(0, ancor);
+    }
+};
 
 export const scrollTo = (ancorId = null, isDataLoaded = false, offset = 0) => {
     if (isDataLoaded) {
-        if (!waitScroll) {
-            const ancor = document.querySelector(ancorId) ? document.querySelector(ancorId).getBoundingClientRect().y - document.body.getBoundingClientRect().y + offset : 0;
+        // Verifica se o scroll já está sendo feito
+        if (!scrollOnce) {
+            const scrollYPos = window.pageYOffset || document.documentElement.scrollTop;
 
-            try {
-                window.scroll({
-                    behavior: 'smooth',
-                    left: 0,
-                    top: ancor
-                });
-            } catch (error) {
-                window.scrollTo(0, ancor);
+            let ancor = document.querySelector(ancorId) ? document.querySelector(ancorId).getBoundingClientRect().y - document.body.getBoundingClientRect().y + offset : 0;
+            let timer = 0;
+
+            // Se o scroll deve ir para um elemento, então é adicionado um timer de 1 segundo para fazer o scroll corretamente
+            if (ancorId && Math.trunc(ancor) !== Math.trunc(scrollYPos)) {
+                timer = 1000;
             }
 
-            waitScroll = true;
+            setTimeout(() => {
+                // Busca a posição da âncora novamente depois do timer, para o valor ser correto
+                ancor = document.querySelector(ancorId) ? document.querySelector(ancorId).getBoundingClientRect().y - document.body.getBoundingClientRect().y + offset : 0;
+
+                fnScroll(ancor);
+            }, timer);
+
+            scrollOnce = true;
         }
 
+        // Reseta a verificação se o scroll já estava sendo feito
         setTimeout(() => {
-            waitScroll = false;
+            scrollOnce = false;
         }, 1000);
     }
 
