316d886c82a8445fede551f3e001368c4713504c
Kevin Veiga <kevin.veiga@gmail.com>
2019-09-26 14:33:28 -0300

    gradient functions

--- src/style/function.js
+++ src/style/function.js
@@ -2,6 +2,23 @@ import { css } from 'styled-components';
 
 import { variable } from './variable';
 
+export const gradientDirectional = (deg = '45deg', startColor = variable.colorPrimary, startColorPercent = '0%', endColor = variable.colorSecondary, endColorPercent = '100%') => {
+    return css`
+        background-image: -webkit-linear-gradient(${deg}, ${startColor} ${startColorPercent}, ${endColor} ${endColorPercent}); // Safari 5.1-6, Chrome 10+
+        background-image: -o-linear-gradient(${deg}, ${startColor} ${startColorPercent}, ${endColor} ${endColorPercent}); // Opera 12
+        background-image: linear-gradient(${deg}, ${startColor} ${startColorPercent}, ${endColor} ${endColorPercent}); // Standard, IE10, Firefox 16+, Opera 12.10+, Safari 7+, Chrome 26+
+        background-repeat: repeat-x;
+    `;
+};
+
+export const gradientRadial = (innerColor = variable.colorPrimary, outerColor = variable.colorSecondary) => {
+    return css`
+        background-image: -webkit-radial-gradient(circle, ${innerColor}, ${outerColor});
+        background-image: radial-gradient(circle, ${innerColor}, ${outerColor});
+        background-repeat: no-repeat;
+    `;
+};
+
 export const letterSpacing = (value = '0', align = '') => {
     return css`
         letter-spacing: ${value};
