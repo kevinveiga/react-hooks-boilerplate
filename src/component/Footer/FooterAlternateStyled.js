5b83239dfb0d3ca33e378319e9b573c54be263a7
Kevin Veiga <kevin.veiga@gmail.com>
2019-10-09 11:40:59 -0300

    update

--- src/component/Footer/FooterAlternateStyled.js
+++ src/component/Footer/FooterAlternateStyled.js
@@ -5,6 +5,7 @@ import { variable } from '../../style/variable';
 export const FooterAlternateStyled = styled.footer`
     background-color: ${variable.colorGrayLight2};
     height: 50px;
+    overflow-y: hidden;
     width: 100%;
     z-index: 3;
 `;
