86256eb64204587e2f2be1dd1d5d9ca6e17f7b96
Kevin Veiga <kevin.veiga@gmail.com>
2019-10-11 12:05:43 -0300

    ajuste em rotas

--- src/router.js
+++ src/router.js
@@ -4,9 +4,10 @@ import { matchPath, Route, Switch, withRouter } from 'react-router-dom';
 import { Context } from './store/context';
 
 import { Cadastro } from './component/Page/Cadastro/Cadastro';
-import { Contato } from './component/Page/Contato/Contato';
 import { Home } from './component/Page/Home/Home';
+import { Login } from './component/Page/Login/Login';
 import { MinhaConta } from './component/Page/MinhaConta/MinhaConta';
+import { MinhaContaCursos } from './component/Page/MinhaConta/MinhaContaCursos';
 import { Noticia } from './component/Page/Noticia/Noticia';
 import { Noticias } from './component/Page/Noticia/Noticias';
 import { Pesquisa } from './component/Page/Pesquisa/Pesquisa';
@@ -16,11 +17,11 @@ export const Router = withRouter((props) => {
     const { setStateHideFooterGlobal, setStateHideHeaderGlobal } = useContext(Context);
 
     // ACTION
-    // Oculta Header e Footer padrões do site de acordo com o array do path
+    // Oculta Header e Footer padrões do site de acordo com array do path
     useEffect(() => {
         if (
             matchPath(window.location.pathname, {
-                path: ['/cadastro', '/minha-conta']
+                path: ['/cadastro', '/login', '/minha-conta']
             })
         ) {
             setStateHideFooterGlobal(true);
@@ -31,16 +32,60 @@ export const Router = withRouter((props) => {
         }
     }, [props, setStateHideFooterGlobal, setStateHideHeaderGlobal]);
 
+    // ROUTES
+    const routes = [
+        {
+            component: () => <Cadastro />,
+            label: 'Cadastro',
+            path: '/cadastro'
+        },
+        {
+            component: () => <Home />,
+            label: 'Home',
+            path: '/inicio'
+        },
+        {
+            component: () => <Login />,
+            label: 'Login',
+            path: '/login'
+        },
+        {
+            component: () => <MinhaConta />,
+            label: 'Minha Conta',
+            path: '/minha-conta/inicio'
+        },
+        {
+            component: () => <MinhaContaCursos />,
+            label: 'Cursos',
+            path: '/minha-conta/cursos'
+        },
+        {
+            component: () => <Noticia />,
+            label: 'Notícia',
+            path: '/noticia/:slug'
+        },
+        {
+            component: () => <Noticias />,
+            label: 'Notícias',
+            path: '/noticias'
+        },
+        {
+            component: () => <Pesquisa />,
+            label: 'Pesquisa',
+            path: '/pesquisa/:slug'
+        },
+        {
+            component: () => <Home />,
+            label: 'Página não encontrada',
+            path: '*'
+        }
+    ];
+
     return (
         <Switch>
-            <Route path="/cadastro" component={Cadastro} />
-            <Route path="/inicio" component={Home} />
-            <Route path="/contato" component={Contato} />
-            <Route path="/minha-conta" component={MinhaConta} />
-            <Route path="/noticia/:slug" component={Noticia} />
-            <Route path="/noticias" component={Noticias} />
-            <Route path="/pesquisa/:slug" component={Pesquisa} />
-            <Route path="*" component={Home} />
+            {routes.map((routes) => (
+                <Route component={routes.component} label={routes.label} key={routes.path} path={routes.path} />
+            ))}
         </Switch>
     );
 });
