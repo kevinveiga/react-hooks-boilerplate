a5e29106a89e7f62980b0e2cb431703ab68288ac
Kevin Veiga <kevin.veiga@gmail.com>
2020-02-12 16:33:33 -0300

    update quotation

---
+++ src/component/Quotation/Quotation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import socketIOClient from 'socket.io-client';
+
+import { socketUrl } from '../../config';
+
+import { useSocket } from '../../service/socket';
+
+import { QuotationAnimation } from './QuotationAnimation';
+
+import { Grid } from '../../style/grid';
+
+const Quotation = () => {
+    // SOCKET
+    const socket = socketIOClient(socketUrl, { autoConnect: false, reconnectionAttempts: 50, reconnectionDelay: 2000, timeout: 10000 });
+
+    // API
+    const stateSocketData = useSocket(socket, 'quotationData');
+
+    const socketData = JSON.parse(stateSocketData || '{}');
+
+    const socketDataLength = socketData ? Object.keys(socketData).length : 0;
+
+    return socketDataLength > 0 ? (
+        <Grid overflowX="hidden">
+            <QuotationAnimation socketData={socketData} />
+        </Grid>
+    ) : null;
+};
+
+export default Quotation;
