67efb0c17384908b51deb9ef9341921c2b9bd783
Kevin Veiga <kevin.veiga@gmail.com>
2020-07-08 10:40:53 -0300

    update

--- src/util/redirectRule.js
+++ src/util/redirectRule.js
@@ -5,8 +5,9 @@ import { cursoMatricula } from '../service/curso';
 export const redirectRule = (url) => {
     try {
         const cursoId = getStorage('cursoId', 'sessionStorage');
-        const redirectUrl = url || getStorage('redirectUrl', 'sessionStorage');
+        const redirectUrl = url || getStorage('redirectUrl', 'sessionStorage') || '/minha-conta/cursos';
 
+        // Limpa url na sessionStorage, somente depois de passar o valor para redirectUrl
         removeStorage('redirectUrl', 'sessionStorage');
 
         // Verifica se deve usar a função para fazer a matricula com o cursoId e redirecionar para o curso dentro de Minha Conta,
@@ -15,7 +16,7 @@ export const redirectRule = (url) => {
         if (cursoId && redirectUrl === `/minha-conta/curso/${cursoId}`) {
             cursoMatricula(cursoId);
         } else {
-            window.location.pathname = redirectUrl || '/minha-conta/cursos';
+            window.location.pathname = redirectUrl;
         }
     } catch (error) {
         console.error(error);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpackMerge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const common = require('./webpack/webpack.common');
 
@@ -13,4 +13,4 @@ const env = envs[process.env.NODE_ENV || 'development'];
 const envConfig = require(`./webpack/webpack.${env}.js`);
 /* eslint-enable global-require, import/no-dynamic-require */
 
-module.exports = webpackMerge(common, envConfig);
+module.exports = merge(common, envConfig);
