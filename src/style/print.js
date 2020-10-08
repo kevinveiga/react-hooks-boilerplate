2163695d25b462d3c7a6034ffc7e82d14b9abcf8
Kevin Veiga <kevin.veiga@gmail.com>
2020-10-08 15:26:44 -0300

    update

--- src/style/print.js
+++ src/style/print.js
@@ -18,12 +18,10 @@ export const PrintPageBreakAfter = styled.div`
     ${printShow};
 
     page-break-after: always;
-    page-break-inside: avoid;
 `;
 
 export const PrintPageBreakBefore = styled.div`
     ${printShow};
 
     page-break-before: always;
-    page-break-inside: avoid;
 `;
