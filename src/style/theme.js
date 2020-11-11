ea8a5da223c4fb4a8c3b4c492330363bae43db2c
Kevin Veiga <kevin.veiga@gmail.com>
2020-11-11 14:25:49 -0300

    update

--- src/style/theme.js
+++ src/style/theme.js
@@ -2,5 +2,5 @@ import { variable } from './variable';
 
 export const theme = {
     breakpoints: { xs: variable.xs, sm: variable.sm, md: variable.md, lg: variable.lg, xl: variable.xl },
-    space: [0, 4, 8, 16, 32, 52]
+    space: [0, 4, 8, 16, 32, 50]
 };
