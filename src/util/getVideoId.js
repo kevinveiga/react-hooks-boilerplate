027dc80c3013297ae4e93100a616d49d2525c81b
Kevin M. Veiga <kevin.veiga@gmail.com>
2021-03-05 09:10:24 -0300

    fix regex-vimeo

--- src/util/getVideoId.js
+++ src/util/getVideoId.js
@@ -1,6 +1,6 @@
 export const getVideoId = (url = '') => {
     const regex = new RegExp('youtube');
-    const results = regex.test(url) ? /.+v=([\w-]{11})/ : /.+([\d]{9})/;
+    const results = regex.test(url) ? /.+v=([\w-]{11})/ : /.+\/(\w+)(\d+)?/;
 
     return url.match(results) && url.match(results)[1];
 };
